08aea4b0e5359dae94f822946425b8f126a62085
Mikayda Mills <mills.ma12@gmail.com>
2019-08-11 21:57:55 -0400

    add hero

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,17 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `limelight`,
+          `source sans pro\:300,400,400i,700`, // you can also specify font weights and styles
+          `roboto mono\:100,300,400,700`,
+        ],
+        display: "swap",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -5,8 +5,18 @@ import "./style.css"
 
 export const Hero = () => {
   return (
-    <div className="c_hero">
-      <h1>hero</h1>
+    <div className="c-hero__container">
+      <div className="c-hero__wrapper--orange">
+        <div className="c-hero__wrapper--white">
+          <h1>D</h1>
+          <h3>DAM-MAD</h3>
+          <h1>M</h1>
+        </div>
+      </div>
+
+      <h1>SOFTWARE</h1>
+      <h1>+</h1>
+      <h1>DESIGN</h1>
     </div>
   )
 }
