cf0fa066548801a3f8dcd189b7c8415caffcd9f0
Mikayda Mills <mills.ma12@gmail.com>
2019-08-14 22:17:24 -0400

    add href to hero

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,9 @@ import "./style.css"
 export const Footer = ({ siteTitle }) => (
   <footer>
     <h4>
-      made with <span>🤬</span> maddness
+      <a href="https://github.com/dam-mad/magistrate">
+        made with <span>🤬</span> maddness
+      </a>
     </h4>
   </footer>
 )
